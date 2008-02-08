68478d16e3bd2100b0521c8b66c860f30758d861
martinea <martinea@a8d146d6-cc15-0410-8900-af154a0219e0>
2008-02-08 02:25:53 +0000

    Tag for amanda-2.6.0b2

---
+++ amanda/tags/amanda260b2/installcheck/amgetconf.pl
@@ -0,0 +1,231 @@
+# Copyright (c) 2006 Zmanda Inc.  All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+#
+# Contact information: Zmanda Inc, 505 N Mathlida Ave, Suite 120
+# Sunnyvale, CA 94085, USA, or: http://www.zmanda.com
+
+use Test::More tests => 48;
+
+use lib "@amperldir@";
+use Installcheck::Config;
+use Installcheck::Run qw(run run_get run_err);
+use Amanda::Paths;
+use Cwd;
+
+# this is re-created for each test
+my $testconf;
+
+##
+# First, try amgetconf out without a config
+
+ok(!run('amgetconf'), "bare amgetconf fails");
+like($Installcheck::Run::stdout, qr(\AUsage: )i, 
+    ".. and gives usage message on stdout");
+like(run_err('amgetconf', 'this-probably-doesnt-exist', 'tapedev'),
+    qr(could not open conf file)i, 
+    "error message when configuration parameter doesn't exist");
+
+##
+# Next, work against a basically empty config
+
+$testconf = Installcheck::Config->new();
+$testconf->write();
+
+# test some defaults
+is(run_get('amgetconf', 'TESTCONF', "reserve"), "100", 
+    "reserve defaults to 100");
+is(run_get('amgetconf', 'TESTCONF', "tapelist"), "tapelist", 
+    "tapelist defaults to 'tapelist'");
+is(run_get('amgetconf', 'TESTCONF', "usetimestamps"), "yes", 
+    "usetimestamps defaults to 'yes'");
+
+# test command-line parsing
+is(run_get('amgetconf', 'TESTCONF', '-o', 'reserve=50', 'reserve'), "50",
+    "-o reserve=50");
+is(run_get('amgetconf', 'TESTCONF', '-oreserve=50', 'reserve'), "50",
+    "-oreserve=50");
+is(run_get('amgetconf', '-o', 'reserve=50', 'TESTCONF', 'reserve'), "50",
+    "-oreserve=50 before config name");
+is(run_get('amgetconf', 'TESTCONF', 'reserve', 'a', 'table', 'for', 'two', '-o', 'reserve=50'), "50",
+    "extra command-line arguments are ignored");
+
+# test a nonexistent parameter
+like(run_err('amgetconf', 'TESTCONF', "foos_per_bar"), qr/no such parameter/, 
+    "handles nonexistent parameters as an error");
+like(run_err('amgetconf', 'TESTCONF', "build.foos_per_bar"), qr/no such parameter/, 
+    "handles nonexistent build parameters");
+
+# Test build parameters that we can determine easily.  Testing all parameters
+# would be more of a maintenance bother than a help.
+is(run_get('amgetconf', 'TESTCONF', "build.bindir"), $bindir,
+    "build.bindir is correct");
+is(run_get('amgetconf', 'TESTCONF', "build.sbindir"), $sbindir,
+    "build.sbindir is correct");
+is(run_get('amgetconf', 'TESTCONF', "build.libexecdir"), $libexecdir,
+    "build.libexecdir is correct");
+is(run_get('amgetconf', 'TESTCONF', "build.amlibexecdir"), $amlibexecdir,
+    "build.amlibexecdir is correct");
+is(run_get('amgetconf', 'TESTCONF', "build.mandir"), $mandir,
+    "build.mandir is correct");
+is(run_get('amgetconf', 'TESTCONF', "build.AMANDA_DBGDIR"), $AMANDA_DBGDIR,
+    "build.AMANDA_DBGDIR is correct");
+is(run_get('amgetconf', 'TESTCONF', "build.AMANDA_TMPDIR"), $AMANDA_TMPDIR,
+    "build.AMANDA_TMPDIR is correct");
+is(run_get('amgetconf', 'TESTCONF', "build.CONFIG_DIR"), $CONFIG_DIR,
+    "build.CONFIG_DIR is correct");
+is(run_get('amgetconf', 'TESTCONF', "build.__empty"), "",
+    "empty build variables handled correctly");
+
+like(run_err('amgetconf', 'TESTCONF', "build.bogus-param"), qr(no such parameter),
+    "bogus build parameters result in an error");
+
+is(run_get('amgetconf', 'TESTCONF', "build.config_dir"), $CONFIG_DIR, 
+    "build parameters are case-insensitive");
+
+is(run_get('amgetconf', "build.bindir"), $bindir, "build variables are available without a config");
+
+# dbopen, dbclose
+my $dbfile = run_get('amgetconf', 'TESTCONF', "dbopen.foo");
+chomp $dbfile;
+like($dbfile, qr(^$AMANDA_DBGDIR/server/foo.[0-9]*.debug$),
+    "'amgetconf dbopen.foo' returns a proper debug filename");
+SKIP: {
+    skip "dbopen didn't work, so I'll skip the rest", 2
+	unless (-f $dbfile);
+    ok(!run('amgetconf', 'TESTCONF', "dbclose.foo"),
+	"dbclose without filename fails");
+    is(run_get('amgetconf', 'TESTCONF', "dbclose.foo:$dbfile"), $dbfile, 
+	"'amgetconf dbclose.foo:<filename>' returns the debug filename");
+}
+
+##
+# Test an invalid config file
+
+$testconf = Installcheck::Config->new();
+$testconf->add_param("foos_per_bar", "10");
+$testconf->write();
+
+like(run_err('amgetconf', 'TESTCONF', "foos_per_bar"), qr/errors processing config file/, 
+    "gives error on invalid configuration");
+
+##
+# Now let's fill in some interesting values
+
+$testconf = Installcheck::Config->new();
+$testconf->add_param("reserved-udp-port", '100,200');
+$testconf->add_param("printer", '"/dev/lp"');
+$testconf->add_param("reserve", '27');
+$testconf->write();
+
+is(run_get('amgetconf', 'TESTCONF', "reserved-udp-port"), "100,200", 
+    "correctly returns intrange parameters from the file");
+is(run_get('amgetconf', 'TESTCONF', "printer"), "/dev/lp", 
+    "correctly returns string parameters from the file");
+is(run_get('amgetconf', 'TESTCONF', "reserve"), "27", 
+    "correctly returns integer parameters from the file");
+is(run_get('amgetconf', 'TESTCONF', "rEsErVe"), "27", 
+    "is case-insensitive");
+
+# check runs without a config
+my $olddir = getcwd();
+chdir("$CONFIG_DIR/TESTCONF") or die("Could not 'cd' to TESTCONF directory");
+is(run_get('amgetconf', "printer"), "/dev/lp", 
+    "uses current directory when no configuration name is given");
+chdir($olddir) or die("Could not 'cd' back to my original directory");
+
+##
+# device_property can appear multiple times
+
+$testconf = Installcheck::Config->new();
+$testconf->add_param("device_property", '"power" "on"');
+$testconf->add_param("device_property", '"turbo" "engaged"');
+$testconf->write();
+
+is_deeply([sort(split(qr/\n/, run_get('amgetconf', 'TESTCONF', 'device_property')))],
+	  [sort('"power" "on"', '"turbo" "engaged"')],
+    "device_property can have multiple values");
+
+##
+# Subsections
+
+$testconf = Installcheck::Config->new();
+$testconf->add_tapetype("cassette", [ length => "32 k" ]);
+$testconf->add_tapetype("reel2reel", [ length => "1 M" ]);
+$testconf->add_tapetype("scotch", [ length => "500 bytes" ]); # (use a sharpie)
+$testconf->add_dumptype("testdump", [ comment => '"testdump-dumptype"' ]);
+$testconf->add_interface("testiface", [ use => '10' ]);
+$testconf->add_holdingdisk("hd17", [ chunksize => '128' ]);
+$testconf->write();
+
+is_deeply([sort(split(/\n/, run_get('amgetconf', 'TESTCONF', '--list', 'tapetype')))],
+	  [sort("cassette", "reel2reel", "scotch", "TEST-TAPE")],
+	"--list returns correct set of tapetypes");
+is(run_get('amgetconf', 'TESTCONF', 'tapetype:scotch:length'), '500', 
+    "returns tapetype parameter correctly");
+
+ok(scalar(grep { $_ eq 'testdump' } 
+	split(/\n/, 
+	    run_get('amgetconf', 'TESTCONF', '--list', 'dumptype'))),
+	"--list returns a test dumptype among the default dumptypes");
+is(run_get('amgetconf', 'TESTCONF', 'dumptype:testdump:comment'), 'testdump-dumptype', 
+    "returns dumptype parameter correctly");
+
+is_deeply([sort(split(/\n/, run_get('amgetconf', 'TESTCONF', '--list', 'interface')))], 
+          [sort("testiface", "default")],
+	"--list returns correct set of interfaces");
+is(run_get('amgetconf', 'TESTCONF', 'interface:testiface:use'), '10', 
+    "returns interface parameter correctly");
+
+is_deeply([sort(split(/\n/, run_get('amgetconf', 'TESTCONF', '--list', 'holdingdisk')))], 
+	  [sort("hd17")], 
+	"--list returns correct set of holdingdisks");
+is(run_get('amgetconf', 'TESTCONF', 'holdingdisk:hd17:chunksize'), '128',
+    "returns holdingdisk parameter correctly");
+
+like(run_get('amgetconf', 'TESTCONF', '--list', 'build'), qr(.*version.*),
+	"'--list build' lists build variables");
+
+# non-existent subsection types, names, and parameters
+like(run_err('amgetconf', 'TESTCONF', 'NOSUCHTYPE:testiface:comment'), qr/no such parameter/, 
+    "handles bad subsection type");
+like(run_err('amgetconf', 'TESTCONF', 'dumptype:NOSUCHDUMP:comment'), qr/no such parameter/, 
+    "handles bad dumptype namek");
+like(run_err('amgetconf', 'TESTCONF', 'dumptype:testdump:NOSUCHPARAM'), qr/no such parameter/, 
+    "handles bad dumptype parameter name");
+
+##
+# exclude lists are a bit funny, too
+
+$testconf = Installcheck::Config->new();
+$testconf->add_dumptype("testdump", [
+    "exclude file optional" => '"f1"', # this optional will have no effect
+    "exclude file append" => '"f2"',
+    "exclude list" => '"l1"',
+    "exclude list append" => '"l2"',
+    "include file" => '"ifo"',
+    "include list optional" => '"ilo"',
+    ]);
+$testconf->write();
+
+is_deeply([sort(split(qr/\n/, run_get('amgetconf', 'TESTCONF', 'dumptype:testdump:exclude')))],
+	  [sort('FILE "f1" "f2"',
+	        'LIST "l1" "l2"')],
+    "exclude files and lists displayed correctly; a non-final optional is ignored");
+
+is_deeply([sort(split(qr/\n/, run_get('amgetconf', 'TESTCONF', 'dumptype:testdump:include')))],
+	  [sort('FILE OPTIONAL "ifo"',
+	        'LIST OPTIONAL "ilo"')],
+    "a final 'OPTIONAL' makes the whole include/exclude optional")
+
---
+++ amanda/tags/amanda260b2/server-src/amcheckdump.pl
@@ -0,0 +1,430 @@
+#! @PERL@
+use lib '@amperldir@';
+use strict;
+
+use File::Basename;
+use Getopt::Long;
+
+use Amanda::Device qw( :constants );
+use Amanda::Debug qw( :logging );
+use Amanda::Config qw( :init :getconf config_dir_relative );
+use Amanda::Logfile;
+use Amanda::Util qw( :running_as_flags );
+use Amanda::Tapelist;
+use Amanda::Changer;
+use Amanda::Constants;
+
+# Have all images been verified successfully so far?
+my $all_success = 1;
+
+sub usage {
+    print <<EOF;
+USAGE:	amcheckdump config [ --timestamp|-t timestamp ] [-o configoption]*
+    amcheckdump validates Amanda dump images by reading them from storage
+volume(s), and verifying archive integrity if the proper tool is locally
+available. amcheckdump does not actually compare the data located in the image
+to anything; it just validates that the archive stream is valid.
+    Arguments:
+	config       - The Amanda configuration name to use.
+	-t timestamp - The run of amdump or amflush to check. By default, check
+			the most recent dump; if this parameter is specified,
+			check the most recent dump matching the given
+			date- or timestamp.
+	-o configoption	- see the CONFIGURATION OVERRIDE section of amanda(8)
+EOF
+    exit(1);
+}
+
+# Find the most recent logfile name matching the given timestamp
+sub find_logfile_name($) {
+    my $timestamp = shift @_;
+    my $rval;
+    my $config_dir = config_dir_relative(getconf($CNF_LOGDIR));
+    # First try log.$datestamp.$seq
+    for (my $seq = 0;; $seq ++) {
+        my $logfile = sprintf("%s/log.%s.%u", $config_dir, $timestamp, $seq);
+        if (-f $logfile) {
+            $rval = $logfile;
+        } else {
+            last;
+        }
+    }
+    return $rval if defined $rval;
+
+    # Next try log.$datestamp.amflush
+    $rval = sprintf("%s/log.%s.amflush", $config_dir, $timestamp);
+
+    return $rval if -f $rval;
+
+    # Finally try log.datestamp.
+    $rval = sprintf("%s/log.%s.amflush", $config_dir, $timestamp);
+    
+    return $rval if -f $rval;
+
+    # No dice.
+    return undef;
+}
+
+## Device management
+
+my $changer_init_done = 0;
+my $current_device;
+my $current_device_label;
+
+sub find_next_device {
+    my $label = shift;
+    if (getconf_seen($CNF_TPCHANGER)) {
+	# We're using a changer script.
+	if (!$changer_init_done) {
+	    my $error = (Amanda::Changer::reset())[0];
+	    die($error) if $error;
+	    $changer_init_done = 1;
+	}
+	my ($error, $slot, $tapedev) = Amanda::Changer::find($label);
+	if ($error) {
+	    die("Error operating changer: $error.");
+	} elsif ($slot eq "<none>") {
+	    die("Could not find tape label $label in changer.");
+	} else {
+	    return $tapedev;
+	}
+    } else {
+	# The user is changing tapes for us.
+	my $device_name = getconf($CNF_TAPEDEV);
+	printf("Insert volume with label %s in device %s and press ENTER: ",
+	       $label, $device_name);
+	<>;
+	return $device_name;
+    }
+}
+
+# Try to open a device containing a volume with the given label.  Returns undef
+# if there is a problem.
+sub try_open_device {
+    my ($label) = @_;
+
+    # can we use the same device as last time?
+    if ($current_device_label eq $label) {
+	return $current_device;
+    }
+
+    # nope -- get rid of that device
+    close_device();
+
+    my $device_name = find_next_device($label);
+    if ( !$device_name ) {
+	print "Could not find a device for label '$label'.\n";
+        return undef;
+    }
+
+    my $device = Amanda::Device->new($device_name);
+    if ( !$device ) {
+	print "Could not open '$device_name'.\n";
+        return undef;
+    }
+
+    $device->set_startup_properties_from_config();
+
+    my $label_status = $device->read_label();
+    if ($label_status != $READ_LABEL_STATUS_SUCCESS) {
+	print "Could not read device $device_name: one of ",
+	     join(", ", ReadLabelStatusFlags_to_strings($label_status)),
+	     "\n";
+	return undef;
+    }
+
+    if ($device->{volume_label} ne $label) {
+	printf("Labels do not match: Expected '%s', but the device contains '%s'.\n",
+		     $label, $device->{volume_label});
+	return undef;
+    }
+
+    if (!$device->start($ACCESS_READ, undef, undef)) {
+	printf("Error reading device %s.\n", $device_name);
+	return undef;
+    }
+
+    $current_device = $device;
+    $current_device_label = $device->{volume_label};
+
+    return $device;
+}
+
+sub close_device {
+    $current_device = undef;
+    $current_device_label = undef;
+}
+
+## Validation application
+
+my ($current_validation_pid, $current_validation_pipeline, $current_validation_image);
+
+# Return a filehandle for the validation application that will handle this
+# image.  This function takes care of split dumps.  At the moment, we have
+# a single "current" validation application, and as such assume that split dumps
+# are stored contiguously and in order on the volume.
+sub open_validation_app {
+    my ($image, $header) = @_;
+
+    # first, see if this is the same image we were looking at previously
+    if (defined($current_validation_image)
+	and $current_validation_image->{timestamp} eq $image->{timestamp}
+	and $current_validation_image->{hostname} eq $image->{hostname}
+	and $current_validation_image->{diskname} eq $image->{diskname}
+	and $current_validation_image->{level} == $image->{level}) {
+	# TODO: also check that the part number is correct
+        print "Continuing with previously started validation process.\n";
+	return $current_validation_pipeline;
+    }
+
+    # nope, new image.  close the previous pipeline
+    close_validation_app();
+	
+    my $validation_command = find_validation_command($header);
+    print "  using '$validation_command'.\n";
+    $current_validation_pid = open($current_validation_pipeline, "|-", $validation_command);
+        
+    if (!$current_validation_pid) {
+	print "Can't execute validation command: $!\n";
+	undef $current_validation_pid;
+	undef $current_validation_pipeline;
+	return undef;
+    }
+
+    $current_validation_image = $image;
+    return $current_validation_pipeline;
+}
+
+# Close any running validation app, checking its exit status for errors.  Sets
+# $all_success to false if there is an error.
+sub close_validation_app {
+    if (!defined($current_validation_pipeline)) {
+	return;
+    }
+
+    # first close the applications standard input to signal it to stop
+    if (!close($current_validation_pipeline)) {
+	my $exit_value = $? >> 8;
+	print "Validation process returned $exit_value (full status $?)\n";
+	$all_success = 0; # flag this as a failure
+    }
+
+    $current_validation_pid = undef;
+    $current_validation_pipeline = undef;
+    $current_validation_image = undef;
+}
+
+# Given a dumpfile_t, figure out the command line to validate.
+sub find_validation_command {
+    my ($header) = @_;
+
+    # We base the actual archiver on our own table, but just trust
+    # whatever is listed as the decrypt/uncompress commands.
+    my $program = uc(basename($header->{program}));
+    
+    my $validation_program;
+    my %validation_programs = (
+        "STAR" => "$Amanda::Constants::STAR -t -f -",
+        "DUMP" => "$Amanda::Constants::RESTORE tbf 2 -",
+        "VDUMP" => "$Amanda::Constants::VRESTORE tf -",
+        "VXDUMP" => "$Amanda::Constants::VXRESTORE tbf 2 -",
+        "XFSDUMP" => "$Amanda::Constants::XFSRESTORE -t -v silent -",
+        "TAR" => "$Amanda::Constants::GNUTAR tf -",
+        "GTAR" => "$Amanda::Constants::GNUTAR tf -",
+        "GNUTAR" => "$Amanda::Constants::GNUTAR tf -",
+        "SMBCLIENT" => "$Amanda::Constants::SAMBA_CLIENT tf -",
+    );
+
+    $validation_program = $validation_programs{$program};
+    if (!defined $validation_program) {
+        warn("Could not determine validation for dumper $program; ".
+             "Will send dumps to /dev/null instead.");
+        $validation_program = "cat > /dev/null";
+    } else {
+        # This is to clean up any extra output the program doesn't read.
+        $validation_program .= " > /dev/null && cat > /dev/null";
+    }
+    
+    my $cmdline = "";
+    if (defined $header->{decrypt_cmd} && 
+        length($header->{decrypt_cmd}) > 0) {
+        $cmdline .= $header->{decrypt_cmd};
+    }
+    if (defined $header->{uncompress_cmd} && 
+        length($header->{uncompress_cmd}) > 0) {
+        $cmdline .= $header->{uncompress_cmd};
+    }
+    $cmdline .= $validation_program;
+
+    return $cmdline;
+}
+
+## Application initialization
+
+Amanda::Util::setup_application("amcheckdump", "server", "cmdline");
+
+my $timestamp = undef;
+my $config_overwrites = new_config_overwrites($#ARGV+1);
+
+Getopt::Long::Configure(qw(bundling));
+GetOptions(
+    'timestamp|t=s' => \$timestamp,
+    'help|usage|?' => \&usage,
+    'o=s' => sub { add_config_overwrite_opt($config_overwrites, $_[1]); },
+) or usage();
+
+usage() if (@ARGV < 1);
+
+my $config_name = shift @ARGV;
+if (!config_init($CONFIG_INIT_EXPLICIT_NAME |
+                 $CONFIG_INIT_FATAL, $config_name)) {
+    die('errors processing config file "' . 
+               Amanda::Config::get_config_filename() . '"');
+}
+apply_config_overwrites($config_overwrites);
+
+Amanda::Util::finish_setup($RUNNING_AS_DUMPUSER);
+
+# Read the tape list.
+my $tl = Amanda::Tapelist::read_tapelist(config_dir_relative(getconf($CNF_TAPELIST)));
+
+# If we weren't given a timestamp, find the newer of
+# amdump.1 or amflush.1 and extract the datestamp from it.
+if (!defined $timestamp) {
+    my $amdump_log = config_dir_relative(getconf($CNF_LOGDIR)) . "/amdump.1";
+    my $amflush_log = config_dir_relative(getconf($CNF_LOGDIR)) . "/amflush.1";
+    my $logfile;
+    if (-f $amflush_log && -f $amdump_log &&
+         -M $amflush_log  < -M $amdump_log) {
+         $logfile=$amflush_log;
+    } elsif (-f $amdump_log) {
+         $logfile=$amdump_log;
+    } elsif (-f $amflush_log) {
+         $logfile=$amflush_log;
+    } else {
+	print "Could not find any dump log file.\n";
+	exit;
+    }
+
+    # extract the datestamp from the dump log
+    open (AMDUMP, "<$logfile") || die();
+    while(<AMDUMP>) {
+	if (/^amdump: starttime (\d*)$/) {
+	    $timestamp = $1;
+	}
+	elsif (/^amflush: starttime (\d*)$/) {
+	    $timestamp = $1;
+	}
+	elsif (/^planner: timestamp (\d*)$/) {
+	    $timestamp = $1;
+	}
+    }
+    close AMDUMP;
+}
+
+# Find all logfiles matching our timestamp
+my @logfiles =
+    grep { $_ =~ /^log\.$timestamp(?:\.[0-9]+|\.amflush)?$/ }
+    Amanda::Logfile::find_log();
+
+if (!@logfiles) {
+    die("Can't find any logfiles with timestamp $timestamp.");
+}
+
+# compile a list of *all* dumps in those logfiles
+my $logfile_dir = config_dir_relative(getconf($CNF_LOGDIR));
+my @images;
+for my $logfile (@logfiles) {
+    push @images, Amanda::Logfile::search_logfile(undef, $timestamp,
+                                                  "$logfile_dir/$logfile", 1);
+}
+
+# filter only "ok" dumps, removing partial and failed dumps
+@images = Amanda::Logfile::dumps_match([@images],
+	undef, undef, undef, undef, 1);
+
+if (!@images) {
+    die("Could not find any matching dumps");
+}
+
+# Find unique tapelist, using a hash to filter duplicate tapes
+my %tapes = map { ($_->{label}, undef) } @images;
+my @tapes = sort { $a cmp $b } keys %tapes;
+
+if (!@tapes) {
+    die("Could not find any matching dumps");
+}
+
+printf("You will need the following tape%s: %s\n", (@tapes > 1) ? "s" : "",
+       join(", ", @tapes));
+
+# Now loop over the images, verifying each one.  
+
+IMAGE:
+for my $image (@images) {
+    # Currently, L_PART results will be n/x, n >= 1, x >= -1
+    # In the past (before split dumps), L_PART could be --
+    # Headers can give partnum >= 0, where 0 means not split.
+    my $logfile_part = 1; # assume this is not a split dump
+    if ($image->{partnum} =~ m$(\d+)/(-?\d+)$) {
+        $logfile_part = $1;
+    }
+
+    printf("Validating image %s:%s datestamp %s level %s part %s on tape %s file #%d\n",
+           $image->{hostname}, $image->{diskname}, $image->{timestamp},
+           $image->{level}, $logfile_part, $image->{label}, $image->{filenum});
+
+    # note that if there is a device failure, we may try the same device
+    # again for the next image.  That's OK -- it may give a user with an
+    # intermittent drive some indication of such.
+    my $device = try_open_device($image->{label});
+    if (!defined $device) {
+	# error message already printed
+	$all_success = 0;
+	next IMAGE;
+    }
+
+    # Now get the header from the device
+    my $header = $device->seek_file($image->{filenum});
+    if (!defined $header) {
+        printf("Could not seek to file %d of volume %s.\n",
+                     $image->{filenum}, $image->{label});
+	$all_success = 0;
+        next IMAGE;
+    }
+
+    # Make sure that the on-device header matches what the logfile
+    # told us we'd find.
+
+    my $volume_part = $header->{partnum};
+    if ($volume_part == 0) {
+        $volume_part = 1;
+    }
+
+    if ($image->{timestamp} ne $header->{datestamp} ||
+        $image->{hostname} ne $header->{name} ||
+        $image->{diskname} ne $header->{disk} ||
+        $image->{level} != $header->{dumplevel} ||
+        $logfile_part != $volume_part) {
+        printf("Details of dump at file %d of volume %s do not match logfile.\n",
+                     $image->{filenum}, $image->{label});
+	$all_success = 0;
+        next IMAGE;
+    }
+    
+    # get the validation application pipeline that will process this dump.
+    my $pipeline = open_validation_app($image, $header);
+
+    # send the datastream from the device straight to the application
+    if (!$device->read_to_fd(fileno($pipeline))) {
+        print "Error reading device or writing data to validation command.\n";
+	$all_success = 0;
+	next IMAGE;
+    }
+}
+
+# clean up
+close_validation_app();
+close_device();
+
+exit($all_success? 0 : 1);
